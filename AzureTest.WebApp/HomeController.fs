c94d9ea04fd2d21b956429e462e913620d00e44c
JohnDoeKyrgyz <JohnDoeKyrgyz@gmail.com>
2014-04-02 22:04:52 -0400

    Updating to retrieve SNOTEL data

--- AzureTest.WebApp/HomeController.fs
+++ AzureTest.WebApp/HomeController.fs
@@ -1,10 +1,23 @@
 ﻿namespace AzureTest.WebApp
+open System
+open System.Collections.Generic
 open System.Web.Mvc
+open Snotel
+
+type SnotelStations = {
+    State: string
+    Stations: IEnumerable<string>}
 
 type HomeController() = 
     inherit Controller()
-    member this.Index() = 
-        this.View()
+    member private this.View(model : obj) = base.View(model)
+    member this.Index(state) = 
+        let state = if String.IsNullOrWhiteSpace(state) then "MT" else state
+        async{            
+            let! stations = GetStationsByState state
+            let result = {State = state; Stations = stations}
+            return this.View result }
+        |> Async.StartAsTask
     member this.About() =
         this.View()
     member this.Contact() =
---
+++ AzureTest.WebApp/Snotel.fs
@@ -0,0 +1,26 @@
+﻿namespace AzureTest.WebApp
+
+module Snotel =
+    open Microsoft.FSharp.Data.TypeProviders
+    open System
+    open System.ServiceModel
+    type SnowtelService = WsdlService<"http://www.wcc.nrcs.usda.gov/awdbWebService/services?WSDL">
+    
+    let service = SnowtelService.GetAwdbWebServiceImplPort()
+    let binding = service.DataContext.Endpoint.Binding :?> BasicHttpBinding
+    do
+        binding.MaxReceivedMessageSize <- int64 Int32.MaxValue
+
+    let GetStationsByState state =
+        async {
+            let! result = service.getStationsAsync(null, [|state|], [|"SNOW"|], null, null, 0m, 0m, 0m, 0m, 0m, 0m, null, null, null, false) |> Async.AwaitTask
+            return result.``return``}
+
+    let GetForecast station startDate endDate =
+        async {
+            let! response = service.getAllForecastsForStationAsync(station, startDate, endDate) |> Async.AwaitTask
+            let forecasts = response.``return``
+            let result = [
+                for forecast in forecasts do
+                    yield forecast.elementCd, forecast.coordinatedForecastValues]
+            return result}
